f65169d1a0d43fdbf1091be5be1a7b51951125f3
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2023-11-06 14:40:26 +0100

    On-going dev

--- src/effect-plus/Function.ts
+++ src/effect-plus/Function.ts
@@ -1,15 +1,27 @@
-export const iterate = <Z>(
-	initial: Z,
+import { Predicate } from 'effect';
+export const iterate: {
+	<A, B extends A>(
+		initial: A,
+		options: {
+			readonly while: Predicate.Refinement<A, B>;
+			readonly body: (b: B) => A;
+		}
+	): A;
+	<A>(
+		initial: A,
+		options: {
+			readonly while: Predicate.Predicate<A>;
+			readonly body: (a: A) => A;
+		}
+	): A;
+} = <A>(
+	initial: A,
 	options: {
-		readonly while: (z: Z) => boolean;
-		readonly body: (z: Z) => Z;
+		readonly while: Predicate.Predicate<A>;
+		readonly body: (a: A) => A;
 	}
 ) => {
-	let z = initial;
-	while (options.while(z)) z = options.body(z);
-	return z;
+	let a = initial;
+	while (options.while(a)) a = options.body(a);
+	return a;
 };
-
-export function isObject(o: unknown): o is object {
-	return typeof o === 'object';
-}
